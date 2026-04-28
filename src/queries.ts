b7aa714d0c652568d272576267e5759e83148fdf
newmediapilot <marcin@newmediapilot.com>
2026-04-27 22:15:31 -0400

    Update queries.ts

--- src/queries.ts
+++ src/queries.ts
@@ -1,4 +1,5 @@
 
-export const alphaQuery = async () => ({ name: "alpha", value: Math.random() })
-export const betaQuery = async () => ({ name: "beta", value: Math.random() })
-export const centauriQuery = async () => ({ name: "centauri", value: Math.random() })
+
+export const alphaQuery = async () => ({ name: "alpha", value: Math.random() });
+export const betaQuery = async () => ({ name: "beta", value: Math.random() });
+export const centauriQuery = async () => ({ name: "centauri", value: Math.random() });
