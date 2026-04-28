a737ee30d644af484a7d3ffc8bfffcda337b913f
newmediapilot <marcin@newmediapilot.com>
2026-04-28 00:02:05 -0400

    Update ApiPhaseAlpha.ts

--- src/queries/ApiPhaseAlpha.ts
+++ src/queries/ApiPhaseAlpha.ts
@@ -1,8 +1,10 @@
 import {APIRegistry} from '../../registry.api.ts';
 
-const {queryKey: name} = APIRegistry["/api/phase/alpha"]
+const {queryKey: name} = APIRegistry["/api/phase/alpha"];
+
+const fixture = await import("./ApiPhaseAlpha.fixture.json");
 
 export async function queryFn() {
     await new Promise((r) => setTimeout(r, 500))
-    return {name, value: Math.random()}
+    return {name, fixture};
 }
