e15d85d3cc476b7d770816349c8d74650db91714
newmediapilot <marcin@newmediapilot.com>
2026-04-28 00:02:03 -0400

    Update PageAlpha.tsx

--- src/PageAlpha.tsx
+++ src/PageAlpha.tsx
@@ -1,9 +1,27 @@
-import {useQuery} from "@tanstack/react-query"
-import {APIRegistry} from "../registry.api"
-import {queryFn} from './queries/ApiPhaseAlpha.ts';
-const {queryKey} = APIRegistry["/api/phase/alpha"]
+import {useQuery} from "@tanstack/react-query";
+import {APIRegistry} from "../registry.api";
+import {queryFn} from "./queries/ApiPhaseAlpha";
+
+const {queryKey} = APIRegistry["/api/phase/alpha"];
 
 export function PageAlpha() {
-    const query = useQuery({ queryKey:[queryKey,{id: 99}], queryFn })
-    return <div>alpha {JSON.stringify(query.data)}</div>
+    const query = useQuery({
+        queryKey: [queryKey, {id: 99}],
+        queryFn,
+    });
+    if (query.isLoading) {
+        return <div>loading...</div>;
+    } else if (query.isError) {
+        return <div>error</div>;
+    } else {
+        return (
+            <div>
+                <div>alpha page</div>
+                <div>status: {String(query.status)}</div>
+                <div>fetching: {String(query.isFetching)}</div>
+                <div>data:</div>
+                <pre>{JSON.stringify(query.data, null, 2)}</pre>
+            </div>
+        );
+    }
 }
