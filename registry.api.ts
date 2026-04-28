204d267e93553732c13848ff9122c9df16d535b8
newmediapilot <marcin@newmediapilot.com>
2026-04-28 00:57:31 -0400

    Update registry.api.ts

--- registry.api.ts
+++ registry.api.ts
@@ -3,7 +3,7 @@
  */
 
 export const APIRegistry = {
-    "/api/phase/alpha":         { queryKey: "/api/phase/alpha",           url: "/api/phase/alpha" },
+       "/api/phase/alpha":         { queryKey: "/api/phase/alpha",           url: "/api/phase/alpha" },
     // "/api/version/beta":        { queryKey: "/api/version/beta",          url: "/api/version/beta" },
     // "/api/codename/centauri":   { queryKey: "/api/codename/centauri",     url: "/api/codename/centauri" }
 } as const
