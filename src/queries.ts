4e1c3b6a372bd21a80431a0ad7c280def602e52a
newmediapilot <marcin@newmediapilot.com>
2026-04-27 23:05:19 -0400

    Delete queries.ts

--- src/queries.ts
+++
@@ -1,5 +0,0 @@
-
-
-export const alphaQuery = async () => ({ name: "alpha", value: Math.random() });
-export const betaQuery = async () => ({ name: "beta", value: Math.random() });
-export const centauriQuery = async () => ({ name: "centauri", value: Math.random() });
